39e119f3840a4d8077d22134e36f49e634138d07
harizitz <43085260+harizitz@users.noreply.github.com>
2020-08-22 15:06:20 +0530

    commit

---
+++ build/precache-manifest.a70fd9d54414ead491717eefb971de02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55d014f330e56982a37814e97ef5fc30",
+    "url": "/recipe-finder-app/index.html"
+  },
+  {
+    "revision": "95dc7be0c585e5e3f138",
+    "url": "/recipe-finder-app/static/css/main.7dbcf09d.chunk.css"
+  },
+  {
+    "revision": "1e66353be373b6e95eb2",
+    "url": "/recipe-finder-app/static/js/2.393fa024.chunk.js"
+  },
+  {
+    "revision": "95dc7be0c585e5e3f138",
+    "url": "/recipe-finder-app/static/js/main.a247d6a2.chunk.js"
+  },
+  {
+    "revision": "b5085258df257703ddaf",
+    "url": "/recipe-finder-app/static/js/runtime~main.0958b62e.js"
+  }
+]);
